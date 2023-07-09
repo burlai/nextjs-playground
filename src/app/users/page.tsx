6d80d9663ed26705dbd1cdd8c59bd1ccff45ec90
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2023-07-09 15:49:57 +0200

    add changing user

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -9,7 +9,11 @@ export default async function Users() {
     <section>
       <h2>Users</h2>
       {usersData.data.map((user: any) => (
-        <Link className="link d-block" key={user.id} href={`/users/${user.id}`}>
+        <Link
+          className="link d-block m-bottom-5"
+          key={user.id}
+          href={`/users/${user.id}`}
+        >
           <span>{user.first_name}</span>
           <span>{user.last_name}</span>
         </Link>
