71bc8dc75c515713cf68e76744908e4bba62a0b8
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2023-07-09 16:11:41 +0200

    add users folder

---
+++ src/app/users/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default async function Users() {
+  const usersResponse = await fetch("https://reqres.in/api/users");
+  const usersData = await usersResponse.json();
+
+  console.log(usersData.data);
+  return (
+    <section>
+      <h2 className="m-bottom-20">Users</h2>
+      {usersData.data.map((user: any) => (
+        <Link
+          className="link d-block m-bottom-5"
+          key={user.id}
+          href={`/users/${user.id}`}
+        >
+          <span>{user.first_name}</span>
+          <span>{user.last_name}</span>
+        </Link>
+      ))}
+    </section>
+  );
+}
